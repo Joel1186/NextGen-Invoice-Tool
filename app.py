46bea0d2c0a2cb283af798b5190713fcb569bf0b
Joel1186 <recovernow2@gmail.com>
2025-06-09 00:45:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,8 @@ def generate_invoice():
     except Exception as e:
         return f"An error occurred while generating the PDF: {str(e)}", 500
 
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
